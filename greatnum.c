ba70acdaf9b1c7501199b1c2004019899f6d1d3d
S-poovarasan <163829919+S-poovarasan@users.noreply.github.com>
2024-12-16 15:35:37 +0530

    Create greatnum.c

---
+++ greatnum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d\n",&a);
+    scanf("%d\n",&b);
+    if(a>b)//check which number is greater
+    printf("%d is greater",a);
+    else
+    printf("%d is greater",b);
+    return 0;
+}
